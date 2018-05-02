4cea70c07ca78fb9e118046866137e1383a81e89
Dougal J. Sutherland <dougal@gmail.com>
2018-05-02 01:31:32 -0700

    fix dump_dir script

--- recipe/dump_dir.py
+++ recipe/dump_dir.py
@@ -3,6 +3,6 @@ import os
 
 import cyflann.flann_info
 
-pth = os.path.join(os.path.dirname(cyflann.flann_info), 'flann_config.json')
+pth = os.path.join(os.path.dirname(cyflann.flann_info.__file__), 'flann_config.json')
 with open(pth, 'w') as f:
     json.dump({'FLANN_DIR': os.environ['FLANN_DIR']}, f)
